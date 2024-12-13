84a2cade787e9e11fe1de3265134e2d986e93c5b
Lennard Scheibel <contact@lennardscheibel.de>
2024-12-13 16:20:32 +0100

    Better error printing

--- main.cu
+++ main.cu
@@ -18,7 +18,8 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
 {
     if (result)
     {
-        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " << file << ":" << line << " '" << func << "' \n";
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << ":" << cudaGetErrorString(result) << " at " << file << ":" << line << " '" << func << "' \n";
+
         // Make sure we call CUDA Device Reset before exiting
         cudaDeviceReset();
         exit(99);
